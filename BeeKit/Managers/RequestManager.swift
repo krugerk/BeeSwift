f6a85584f9418bd78e5d123b16f9ffd3fceb6885
krugerk <4656811+krugerk@users.noreply.github.com>
2026-02-01 16:05:56 +0100

    clean code

--- BeeKit/Managers/RequestManager.swift
+++ BeeKit/Managers/RequestManager.swift
@@ -11,29 +11,6 @@ import Foundation
 import OSLog
 import SwiftyJSON
 
-public enum ServerError: LocalizedError {
-  case notFound
-  case unauthorized
-  case forbidden
-  case serverError(Int)
-  case custom(String, requestError: Error?)
-  public var errorDescription: String? {
-    switch self {
-    case .notFound: return "Not found"
-    case .unauthorized: return "Unauthorized"
-    case .forbidden: return "Permission denied"
-    case .serverError(let code): return "Server error (\(code)). Please try again later"
-    case .custom(let message, _): return message
-    }
-  }
-  var requestError: Error? {
-    switch self {
-    case .custom(_, let error): return error
-    default: return nil
-    }
-  }
-}
-
 
 public protocol RequestManaging {
   func get(url: String, parameters: [String: Any]?) async throws -> Any?
